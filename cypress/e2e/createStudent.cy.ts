7dabe903e046d8ec5550bdb72d751f293cf85ac9
Brenda <abrendakhenya@gmail.com>
2024-06-20 11:53:45 -0400

    feat: automation new student

--- cypress/e2e/createStudent.cy.ts
+++ cypress/e2e/createStudent.cy.ts
@@ -1,4 +1,5 @@
-// cypress/support/commands.js
+/// <reference types="cypress" />
+
 const email = 'paulwilkerlf@gmail.com';
 const password = '123456';
 
@@ -9,8 +10,13 @@ Cypress.Commands.add('login', (email, password) => {
     cy.contains('Login').click();
 });
   
-// cypress/e2e/your_test_file.cy.js
+
 describe('Create new student', () => {   
+    const code = Math.floor(Math.random() * 100000);
+    const name = `Student${code}`;
+    const lastname = `Student${code}`;
+    
+
     beforeEach(() => {
       cy.login(email, password);
     });
@@ -18,6 +24,15 @@ describe('Create new student', () => {
     it('Should create a new student successfully', () => {
         cy.get('[data-testid="MenuIcon"]').should('be.visible').click();
         cy.get('[data-testid="SchoolOutlinedIcon"]').should('be.visible').click();
+        cy.get('.MuiButton-containedSecondary.css-kcsbbo-MuiButtonBase-root-MuiButton-root').should('be.visible').click();
+        // insert data
+        cy.get('#name').should('be.visible').type(name);
+        cy.get('#lastname').should('be.visible').type(lastname);
+        cy.get('#code').should('be.visible').type(code);
+        cy.get('#degree').should('be.visible').click();
+
+        
+        //cy.get('[data-testid="ChecklistOutlinedIcon"]').should('be.visible').click();
     });
 });
   
