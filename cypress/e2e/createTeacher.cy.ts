0521d11b7fb036563d425894a6ee668a3802cb41
Brenda <abrendakhenya@gmail.com>
2024-06-23 11:51:37 -0400

    feat: automation create a new teacher

--- cypress/e2e/createTeacher.cy.ts
+++ cypress/e2e/createTeacher.cy.ts
@@ -13,8 +13,8 @@ Cypress.Commands.add('login', (email, password) => {
 
 describe('Create new student', () => {   
     const code = Math.floor(Math.random() * 100000);
-    const name = `Student${code}`;
-    const lastname = `Student${code}`;
+    const name = `Teacher${code}`;
+    const lastname = `Teacher${code}`;
     const testEmail = `testEmail${code}@gmail.com`;
     
     
@@ -23,18 +23,50 @@ describe('Create new student', () => {
       cy.login(email, password);
     });
   
-    it('Should create a new teacherwith the position of a licensed successfully', () => {
+    it('Should create a new teacher with the position of a licensed successfully', () => {
         cy.get('[data-testid="MenuIcon"]').should('be.visible').click();
         cy.get('[data-testid="SupervisorAccountIcon"]').should('be.visible').click();
         cy.get('.MuiButton-containedSecondary.css-kcsbbo-MuiButtonBase-root-MuiButton-root').should('be.visible').click();
         // insert data
-        cy.get('#name').should('be.visible').type(name);
-        cy.get('#lastname').should('be.visible').type(lastname);
-        cy.get('#code').should('be.visible').type(code);
+        cy.get('#name').should('be.visible').type(name + 'licensed');
+        cy.get('#lastname').should('be.visible').type(lastname + 'licensed');
+        cy.get('#code').should('be.visible').type(code + '1');
         cy.get('#degree').should('be.visible').click();
         cy.get('[data-value="licenciado"]').should('be.visible').click();
-        cy.get('#email').should('be.visible').type(testEmail);
-        cy.get('#phone').should('be.visible').type(code);
+        cy.get('#email').should('be.visible').type('licensed' + testEmail);
+        cy.get('#phone').should('be.visible').type(code + '1');
+        cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary').should('be.visible').click();
+        cy.get('.MuiAlert-message.css-1pxa9xg-MuiAlert-message').should('be.visible').and('contain', 'Profesor creado con éxito');
+    });
+    
+    it('Should create a new teacher with the position of a maestrer successfully', () => {
+        cy.get('[data-testid="MenuIcon"]').should('be.visible').click();
+        cy.get('[data-testid="SupervisorAccountIcon"]').should('be.visible').click();
+        cy.get('.MuiButton-containedSecondary.css-kcsbbo-MuiButtonBase-root-MuiButton-root').should('be.visible').click();
+        // insert data
+        cy.get('#name').should('be.visible').type(name + 'master');
+        cy.get('#lastname').should('be.visible').type(lastname + 'master');
+        cy.get('#code').should('be.visible').type(code + '2');
+        cy.get('#degree').should('be.visible').click();
+        cy.get('[data-value="maestro"]').should('be.visible').click();
+        cy.get('#email').should('be.visible').type('master' + testEmail);
+        cy.get('#phone').should('be.visible').type(code + '2');
+        cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary').should('be.visible').click();
+        cy.get('.MuiAlert-message.css-1pxa9xg-MuiAlert-message').should('be.visible').and('contain', 'Profesor creado con éxito');
+    });
+    
+    it('Should create a new teacher with the position of a PhD successfully', () => {
+        cy.get('[data-testid="MenuIcon"]').should('be.visible').click();
+        cy.get('[data-testid="SupervisorAccountIcon"]').should('be.visible').click();
+        cy.get('.MuiButton-containedSecondary.css-kcsbbo-MuiButtonBase-root-MuiButton-root').should('be.visible').click();
+        // insert data
+        cy.get('#name').should('be.visible').type(name + 'PhD');
+        cy.get('#lastname').should('be.visible').type(lastname + 'PhD');
+        cy.get('#code').should('be.visible').type(code + '3');
+        cy.get('#degree').should('be.visible').click();
+        cy.get('[data-value="maestro"]').should('be.visible').click();
+        cy.get('#email').should('be.visible').type('PhD' + testEmail);
+        cy.get('#phone').should('be.visible').type(code + '3');
         cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary').should('be.visible').click();
         cy.get('.MuiAlert-message.css-1pxa9xg-MuiAlert-message').should('be.visible').and('contain', 'Profesor creado con éxito');
     });
