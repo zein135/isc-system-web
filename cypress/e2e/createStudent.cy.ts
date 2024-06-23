9f502622cbdc454579866082ec9a25a42c1fce2a
Brenda <abrendakhenya@gmail.com>
2024-06-23 12:19:09 -0400

    feat: automation create a new student

--- cypress/e2e/createStudent.cy.ts
+++ cypress/e2e/createStudent.cy.ts
@@ -1,15 +1,20 @@
+// cypress/support/index.d.ts
 /// <reference types="cypress" />
+import './commands';
 
 const email = 'paulwilkerlf@gmail.com';
 const password = '123456';
 
 Cypress.Commands.add('login', (email, password) => {
+    if (typeof email !== 'string' || typeof password !== 'string') {
+        throw new Error('Email and password must be strings');
+    }
+
     cy.visit('http://localhost:5173/login');
     cy.get('input[name=email]').type(email);
     cy.get('input[name=password]').type(password);
     cy.contains('Login').click();
 });
-  
 
 describe('Create new student', () => {   
     const code = Math.floor(Math.random() * 100000);
@@ -28,12 +33,10 @@ describe('Create new student', () => {
         // insert data
         cy.get('#name').should('be.visible').type(name);
         cy.get('#lastname').should('be.visible').type(lastname);
-        cy.get('#code').should('be.visible').type(code);
+        cy.get('#code').should('be.visible').type(code.toString());
         cy.get('#degree').should('be.visible').click();
         cy.get('[data-value="licenciado"]').should('be.visible').click();
 
-        
-        //cy.get('[data-testid="ChecklistOutlinedIcon"]').should('be.visible').click();
     });
 });
   
