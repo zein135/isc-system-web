f3be617512044546160492c76f70d05850a782a1
Brenda <abrendakhenya@gmail.com>
2024-06-24 21:37:50 -0400

    feat: automation create a new student

--- cypress/e2e/createStudent.cy.ts
+++ cypress/e2e/createStudent.cy.ts
@@ -1,19 +1,18 @@
-// cypress/support/index.d.ts
 /// <reference types="cypress" />
-import './commands';
 
 const email = 'paulwilkerlf@gmail.com';
 const password = '123456';
 
-Cypress.Commands.add('login', (email, password) => {
-    if (typeof email !== 'string' || typeof password !== 'string') {
-        throw new Error('Email and password must be strings');
-    }
 
-    cy.visit('http://localhost:5173/login');
-    cy.get('input[name=email]').type(email);
-    cy.get('input[name=password]').type(password);
-    cy.contains('Login').click();
+Cypress.Commands.add('login', (email, password) => {
+  if (typeof email !== 'string' || typeof password !== 'string') {
+      throw new Error('Email and password must be strings');
+  }
+
+  cy.visit('http://localhost:5173/login');
+  cy.get('input[name=email]').type(email);
+  cy.get('input[name=password]').type(password);
+  cy.contains('Login').click();
 });
 
 describe('Create new student', () => {   
