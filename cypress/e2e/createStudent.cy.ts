58270cdb9a1447f229d12a3f8e6128c7619a3223
Brenda <abrendakhenya@gmail.com>
2024-07-08 19:22:14 -0400

    feat: automation create a new student

--- cypress/e2e/createStudent.cy.ts
+++ cypress/e2e/createStudent.cy.ts
@@ -19,23 +19,71 @@ describe('Create new student', () => {
     const code = Math.floor(Math.random() * 100000);
     const name = `Student${code}`;
     const lastname = `Student${code}`;
-    
+    const emailS = `Student${code}@gmail.`;
 
     beforeEach(() => {
       cy.login(email, password);
     });
   
-    it('Should create a new student successfully', () => {
+    it('Should create a new student licensed successfully', () => {
         cy.get('[data-testid="MenuIcon"]').should('be.visible').click();
         cy.get('[data-testid="SchoolOutlinedIcon"]').should('be.visible').click();
         cy.get('.MuiButton-containedSecondary.css-kcsbbo-MuiButtonBase-root-MuiButton-root').should('be.visible').click();
         // insert data
-        cy.get('#name').should('be.visible').type(name);
-        cy.get('#lastname').should('be.visible').type(lastname);
-        cy.get('#code').should('be.visible').type(code.toString());
+        cy.get('#name').should('be.visible').type(name + 'licensed');
+        cy.get('#lastname').should('be.visible').type(lastname + 'licensed');
+        cy.get('#code').should('be.visible').type(code.toString() + 1);
         cy.get('#degree').should('be.visible').click();
         cy.get('[data-value="licenciado"]').should('be.visible').click();
+        cy.get('#email').type(emailS + 'com');
+        cy.get('#phone').type(code.toString() + 1);
 
+        // save
+        cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary').should('be.visible').click();
+
+        // verify student creation
+        cy.get('.MuiAlert-message.css-1pxa9xg-MuiAlert-message').should('be.visible').and('contain', 'Profesor creado con éxito');
     });
+
+    it('Should create a new student maestrer successfully', () => {
+      cy.get('[data-testid="MenuIcon"]').should('be.visible').click();
+      cy.get('[data-testid="SchoolOutlinedIcon"]').should('be.visible').click();
+      cy.get('.MuiButton-containedSecondary.css-kcsbbo-MuiButtonBase-root-MuiButton-root').should('be.visible').click();
+      // insert data
+      cy.get('#name').should('be.visible').type(name + 'maestrer');
+      cy.get('#lastname').should('be.visible').type(lastname + 'maestrer');
+      cy.get('#code').should('be.visible').type(code.toString() + 2);
+      cy.get('#degree').should('be.visible').click();
+      cy.get('[data-value="maestro"]').should('be.visible').click();
+      cy.get('#email').type(emailS + 'edu');
+      cy.get('#phone').type(code.toString() + 2);
+
+      // save
+      cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary').should('be.visible').click();
+
+      // verify student creation
+      cy.get('.MuiAlert-message.css-1pxa9xg-MuiAlert-message').should('be.visible').and('contain', 'Profesor creado con éxito');
+  });
+
+  it('Should create a new student PhD successfully', () => {
+    cy.get('[data-testid="MenuIcon"]').should('be.visible').click();
+    cy.get('[data-testid="SchoolOutlinedIcon"]').should('be.visible').click();
+    cy.get('.MuiButton-containedSecondary.css-kcsbbo-MuiButtonBase-root-MuiButton-root').should('be.visible').click();
+    // insert data
+    cy.get('#name').should('be.visible').type(name + 'PhD');
+    cy.get('#lastname').should('be.visible').type(lastname + 'PhD');
+    cy.get('#code').should('be.visible').type(code.toString() + 3);
+    cy.get('#degree').should('be.visible').click();
+    cy.get('[data-value="maestro"]').should('be.visible').click();
+    cy.get('#email').type(emailS + 'bo');
+    cy.get('#phone').type(code.toString() + 3);
+
+    // save
+    cy.get('.MuiButtonBase-root.MuiButton-root.MuiButton-containedPrimary').should('be.visible').click();
+
+    // verify student creation
+    cy.get('.MuiAlert-message.css-1pxa9xg-MuiAlert-message').should('be.visible').and('contain', 'Profesor creado con éxito');
+  });
+
 });
   
