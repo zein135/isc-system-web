8921005ca149ded37b5adc8fef5368be0416ecb1
= <=>
2024-08-18 17:21:16 -0400

    headerOfTable

--- src/components/administration/PermissionTable.tsx
+++ src/components/administration/PermissionTable.tsx
@@ -1,5 +1,24 @@
-const PermissionTable = () =>{
+import { Grid, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+const PermissionTable = () =>{   
+return(
+    <Grid item xs={8}>
+    <Table className="border-table">
+      <TableHead className="large-header">
+        <TableRow>
+          <TableCell>
+            Acción
+          </TableCell>
+          <TableCell>
+            Permisos
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
 
+      </TableBody>
+    </Table>
+  </Grid>
+)
 }
 
 export default PermissionTable
