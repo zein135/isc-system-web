3c7204bd308e6586ceebeb17caac9e3e1f83fbb5
= <=>
2024-08-18 17:21:16 -0400

    headerOfTable

--- src/components/administration/RollTable.tsx
+++ src/components/administration/RollTable.tsx
@@ -1,5 +1,20 @@
-const RollTable = () =>{
+import { Grid, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
 
+const RollTable = () =>{
+ return (<Grid item xs={3} >
+    <Table className="border-table">
+      <TableHead className="orange-header large-header">
+        <TableRow>
+          <TableCell>
+            Roles
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+
+      </TableBody>
+    </Table>
+  </Grid>)
 }
 
 export default RollTable
--- src/pages/Administrator/AdministratorPage.tsx
+++ src/pages/Administrator/AdministratorPage.tsx
@@ -1,7 +1,15 @@
-import "../../components/administrator/AdministratorPageComponents.css"
+import "../../components/administration/AdministratorPageComponents.css"
+import { Grid } from "@mui/material";
+import RollTable from "../../components/administration/RollTable";
+import PermissionTable from "../../components/administration/PermissionTable";
 
 const AdminsitratorPage = () => {
     return(<>
+     <Grid container spacing={6}>
+        <RollTable/>
+        <PermissionTable/>
+      </Grid>
+    
     </>);
 }
 
