21ef429acb5624dd9d02334757b6849cdc481b3e
vale_ <valeria.zerain@gmail.com>
2024-08-22 17:27:48 -0400

    Create new role in administrator page

--- src/pages/Administrator/AdministratorPage.tsx
+++ src/pages/Administrator/AdministratorPage.tsx
@@ -1,21 +1,58 @@
-import "../../components/administration/AdministratorPageComponents.css"
-import { Grid, useMediaQuery } from "@mui/material";
+import "../../components/administration/AdministratorPageComponents.css";
+import { Grid, IconButton, useMediaQuery } from "@mui/material";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { useEffect, useState } from "react";
 import RoleTable from "../../components/administration/RoleTable";
 import PermissionTable from "../../components/administration/PermissionTable";
+import AddTextModal from "../../components/common/AddTextModal";
+import { getRoles, addRole } from "../../services/roleService";
 
 const AdminsitratorPage = () => {
-  const isSmallScreen = useMediaQuery('(max-width:600px)'); // ajusta el valor según sea necesario
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [roles, setRoles] = useState([]);
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
 
+  useEffect(() => {
+    const fetchRoles = async () => {
+      try {
+        const rolesData = await getRoles();
+        setRoles(rolesData);
+      } catch (error) {
+        console.error("Error fetching roles:", error);
+      }
+    };
+    fetchRoles();
+  }, []);
+
+  const handleCreate = async (roleName: string) => {
+    try {
+      await addRole({ roleName, id: roles.length + 1 });
+      const updateRoles = await getRoles();
+      setRoles(updateRoles);
+    } catch (error) {
+      console.error("Error creating role:", error);
+    }
+  };
   return (
     <Grid container spacing={6}>
       <Grid item xs={!isSmallScreen ? 3 : 12}>
-        <RoleTable smallSize={!isSmallScreen} />
+        <RoleTable smallSize={!isSmallScreen}/>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '16px' }}>
+          <IconButton aria-label="add" onClick={() => setIsModalVisible(true)}>
+            <AddCircleIcon color="primary" style={{ fontSize: 40 }} />
+          </IconButton>
+        </div>
       </Grid>
       <Grid item xs={8}>
         {isSmallScreen ? null : <PermissionTable />}
       </Grid>
+        <AddTextModal
+          isVisible={isModalVisible}
+          setIsVisible={setIsModalVisible}
+          onCreate={handleCreate}
+        />
     </Grid>
   );
 }
 
-export default AdminsitratorPage
+export default AdminsitratorPage;
