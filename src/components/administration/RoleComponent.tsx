5f7978ab8f9c80b659a05a82e264096aac80e5c1
Miguel <angeloquenta@gmail.com>
2024-08-22 23:10:03 -0400

    feat add button and dialog to confirm delete

--- src/components/administration/RoleComponent.tsx
+++ src/components/administration/RoleComponent.tsx
@@ -1,19 +1,50 @@
-import { Card, CardActionArea, CardContent, Typography } from "@mui/material"
-interface Role {
+import { Box, Button, Card, CardActionArea, CardContent, Icon, Typography } from "@mui/material"
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useState } from "react"
+import ConfirmDelete from "./ConfirmDelete"
+
+type Role = {
     roleName: string
 }
 
-const RoleComponent = ({role}:{role:Role} ) => {
+const RoleComponent = ({ role }: { role: Role }) => {
+
+    const [showDelete, setShowDelete] = useState<boolean>(false)
     return (
-        <Card>
-            <CardActionArea>
-                <CardContent>
-                    <Typography>
-                        {role.roleName}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <>
+            <Card>
+                <CardActionArea>
+                    <CardContent>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Typography>
+                                {role.roleName}
+                            </Typography>
+                            <Button
+                                sx={{
+                                    padding: 0,
+                                    minWidth: 0,
+                                    minHeight: 0,
+                                    width: 24,
+                                    height: 24,
+                                }}
+                                onClick={() => {
+                                    setShowDelete(true)
+                                }}>
+                                <Icon sx={{
+                                    fontSize: 27,
+                                }}>
+                                    <DeleteIcon color="primary" />
+                                </Icon>
+                            </Button>
+                        </Box>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+
+            {showDelete && (
+                <ConfirmDelete roleName={role.roleName} setShowDelete={setShowDelete} />
+            )}
+        </>
     )
 }
 
