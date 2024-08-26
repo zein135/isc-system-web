dd6a4f4c766560252f4f3edaef1503625a1df7bf
Navi <obitoTCP@gmail.com>
2024-08-26 10:51:25 -0400

    feat: US0012 Cambio de color

--- src/pages/ScholarshipHours/GraphicHours.tsx
+++ src/pages/ScholarshipHours/GraphicHours.tsx
@@ -1,10 +1,12 @@
-
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Legend } from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Legend, Cell } from "recharts";
 import Box from "@mui/material/Box";
 import { barData } from "../../data/graphicData";
 
-
 export default function GraphicHours() {
+    const getFillColor = (hours: number): string => {
+        return hours < 50 ? "#FF0000" : "#4CAF50";
+    };
+
     return (
         <Box sx={{ width: "500px" }}>
             <BarChart width={500} height={450} data={barData}>
@@ -13,7 +15,11 @@ export default function GraphicHours() {
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="hours" fill="#4CAF50" />
+                <Bar dataKey="hours">
+                    {barData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={getFillColor(entry.hours)} />
+                    ))}
+                </Bar>
             </BarChart>
         </Box>
     );
