5bc8915d22cf54ed7bf2d1688648449212c98543
Paul Landaeta <paulwilkerlf@gmail.com>
2024-08-29 13:58:43 -0400

    feat: merge TASK002

--- src/routes/AuthGuard.tsx
+++ src/routes/AuthGuard.tsx
@@ -2,8 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const AuthGuard = () => {
   
-  // verificar si el usuario esta logeado o no? 
-  const isAuthenticated = true; // localStorage.getItem("token");
+  const isAuthenticated = localStorage.getItem("token");
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace/>;
