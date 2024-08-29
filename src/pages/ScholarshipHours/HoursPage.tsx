2c62bda2f7c4e6c8fda8e41b7a2d13672df57eba
Ivan Poma <pomaivan503@gmail.com>
2024-08-28 22:02:50 -0400

    fix: US0012 CorrectScreenStyles

--- src/pages/ScholarshipHours/HoursPage.tsx
+++ src/pages/ScholarshipHours/HoursPage.tsx
@@ -1,100 +1,104 @@
+import { Grid, Box, Button, Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import CircularProgress, { CircularProgressProps } from "@mui/material/CircularProgress";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
 import GraphicHours from "./GraphicHours";
-import { colors, labels, progresses } from "../../data/hoursData";
+import HoursCard from "../../components/common/HoursCard";
 
-function CircularProgressWithLabel(props: CircularProgressProps & { value: number, customColor: string }) {
-    return (
-        <Box sx={{ 
-            backgroundColor: props.customColor, 
-            padding: "10px", 
-            borderRadius: "15px", 
-            display: "flex", 
-            alignItems: "center", 
-            justifyContent: "space-between",
-            width: "100%",
-            marginBottom: "20px",
-            position: "relative"
-        }}>
-            <Box sx={{ position: "relative", display: "inline-flex", marginRight: "20px" }}>
-                <CircularProgress
-                    variant="determinate"
-                    {...props}
-                    size={90}
-                    thickness={9}
-                    sx={{
-                        color: "white",
-                    }}
-                />
-            </Box>
-
-            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <Typography variant="h4" component="div" color="white">
-                    {`${Math.round(props.value)} Horas`}
-                </Typography>
-            </Box>
-        </Box>
-    );
+interface HoursStats {
+    totalHorasRequeridas: number;
+    horasRealizadas: number;
+    horasFaltantes: number;
 }
 
-export default function CircularWithValueLabel() {
+function HoursPage() {
     const navigate = useNavigate();
+    const [stats, setStats] = useState<HoursStats>();
+
+    useEffect(() => {
+    setTimeout(() => {
+        setStats({
+        totalHorasRequeridas: 50,
+        horasRealizadas: 0,
+        horasFaltantes: 50,
+        });
+    }, 1000);
+    }, []);
 
     const handleEventsClick = () => {
-        navigate("/events");
+    navigate("/events");
     };
 
     const handleRegistrationEventsClick = () => {
-        navigate("/myEvents");
+    navigate("/myEvents");
     };
 
     return (
-        <Box 
-            sx={{ 
-                display: "flex", 
-                flexDirection: { xs: "column", md: "row" },  
-                justifyContent: "center", 
-                alignItems: "center", 
-                width: "90%",
-                padding: { xs: "10px", md: "20px" },  
-            }}
-        >
-            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", marginRight: { xs: "0", md: "40px" }, width: "100%" }}>
-                {progresses.map((progress, index) => (
-                    <Box key={index} sx={{ width: "100%", maxWidth: "400px" }}>  
-                        <Typography variant="h6" component="div" sx={{ marginBottom: "10px", textAlign: "center" }}>
-                            {labels[index]}
-                        </Typography>
-                        <CircularProgressWithLabel
-                            value={progress}
-                            customColor={colors[index]}
-                        />
-                    </Box>
-                ))}
-                
-                <Box sx={{ display: "flex", justifyContent: "center", marginTop: "20px", flexDirection: { xs: "column", md: "row" } }}>
-                    <Button 
-                        variant="contained" 
-                        color="primary" 
-                        sx={{ margin: "10px", width: "200px" }}
-                        onClick={handleRegistrationEventsClick}                           
-                    >
-                        Pre inscripciones
-                    </Button>
-                    <Button 
-                        variant="contained" 
-                        color="primary" 
-                        sx={{ margin: "10px", width: "200px" }}
-                        onClick={handleEventsClick}
-                    >
-                        Eventos
-                    </Button>
-                </Box>
-            </Box>
+    <Container fixed>
+        <Grid container spacing={8} alignItems="center" justifyContent="center">
+        <Grid item xs={22} md={4}>
+            <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <HoursCard
+                backgroundColor="#f3a43f"
+                textColor="#FFFFFF"
+                title="Total de Horas Requeridas"
+                hours={stats?.totalHorasRequeridas || 0}
+                percentage={(stats?.totalHorasRequeridas || 0) / 1}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <HoursCard
+                backgroundColor="#359be5"
+                textColor="#FFFFFF"
+                title="Horas Realizadas"
+                hours={stats?.horasRealizadas || 0}
+                percentage={(stats?.horasRealizadas || 0) / (stats?.totalHorasRequeridas || 1) * 100}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <HoursCard
+                backgroundColor="#ef4444"
+                textColor="#FFFFFF"
+                title="Horas Faltantes"
+                hours={stats?.horasFaltantes || 0}
+                percentage={(stats?.horasFaltantes || 0) / 1}
+                />
+            </Grid>
+            </Grid>
+        </Grid>
+        <Grid item xs={16} md={6}>
+            <Box sx={{ width: "100%", padding: "20px" }}>
             <GraphicHours />
+            </Box>
+        </Grid>
+        </Grid>
+        <Box
+        sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+            flexDirection: { xs: "column", md: "row" },
+        }}
+        >
+        <Button
+            variant="contained"
+            color="primary"
+            sx={{ margin: "10px", width: { xs: "100%", md: "200px" } }}
+            onClick={handleRegistrationEventsClick}
+        >
+            Pre inscripciones
+        </Button>
+        <Button
+            variant="contained"
+            color="primary"
+            sx={{ margin: "10px", width: { xs: "100%", md: "200px" } }}
+            onClick={handleEventsClick}
+        >
+            Eventos
+        </Button>
         </Box>
+    </Container>
     );
 }
+
+export default HoursPage;
