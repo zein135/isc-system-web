22a7f65007119bc2c69c1f3d4b21cc716ddb1a59
Paul Landaeta <paulwilkerlf@gmail.com>
2024-08-29 16:17:59 -0400

    fix: remove imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
   Params,
   LoaderFunction,
   LoaderFunctionArgs,
 } from "react-router-dom";
 
-import ProcessInfoPage from "./pages/graduation/ProcessInfoPage";
-import ErrorPage from "./pages/ErrorPage";
 import { getProcess, getStudentById } from "./services/processServicer";
-import LoginPage from "./pages/auth/LoginPage";
-import Layout from "./layout/Layout";
-
-import CreateProcessPage from "./pages/CreateGraduation/CreateProcessPage";
-import ProfessorPage from "./pages/Professor/ProfessorPage";
-import CreateProfessorPage from "./pages/Professor/CreateProfessorPage";
-import { RequireAuth } from "./layout/RequireAuth";
-import { DashboardPage } from "./pages/dashboard/Dashboard";
-import StudentPage from "./pages/Student/StudentsPage";
-import CreateStudentPage from "./pages/Student/CreateStudentPage";
-import EditStudentPage from "./pages/Student/EditStudentPage";
-import Profile from "./pages/profile/Profile";
-import GraduationProcessPage from "./pages/graduation/GraduationProcessPage";
-import AdministratorPage from "./pages/Administrator/AdministratorPage";
 
 import publicRoutes from "./routes/PublicRoutes";
 import protectedRoutes from "./routes/ProtectedRoutes";
@@ -46,7 +29,6 @@ const getStudentProcess: LoaderFunction = async ({
 const router = createBrowserRouter([
   ...publicRoutes,
   {
-
     element: <AuthGuard />,
     children: protectedRoutes,
   },
