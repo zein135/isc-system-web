7f80577f17af3d3a364c094002107bcd9122aec8
Paul Landaeta <paulwilkerlf@gmail.com>
2024-08-29 13:11:15 -0400

    feat: add guards to auth and role validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import {
   createBrowserRouter,
-  Navigate,
   RouterProvider,
 } from "react-router-dom";
 
-import ProcessInfoPage from "./pages/graduation/ProcessInfoPage";
-import ErrorPage from "./pages/ErrorPage";
 import { getProcess, getStundentById } from "./services/processServicer";
-import StudentsPage from "./pages/StudentsPage";
-import LoginPage from "./pages/auth/LoginPage";
-import Layout from "./layout/Layout";
 
-import CreateProcessPage from "./pages/CreateGraduation/CreateProcessPage";
-import ProfessorPage from "./pages/ProfessorPage";
-import CreateProfessorPage from "./pages/Professor/CreateProfessorPage";
-import { RequireAuth } from "./layout/RequireAuth";
-import { DashboardPage } from "./pages/dashboard/Dashboard";
-import StudentPage from "./pages/Student/StudentsPage";
-import CreateStudentPage from "./pages/Student/CreateStudentPage";
-import EditStudentPage from "./pages/Student/EditStudentPage";
 
 import publicRoutes from "./routes/PublicRoutes";
 import protectedRoutes from "./routes/ProtectedRoutes";
@@ -40,108 +26,6 @@ const router = createBrowserRouter([
     children: protectedRoutes,
   },
 ]);
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         index: true,
-//         element: <Navigate to="/login" replace />,
-//       },
-//       {
-//         index: true,
-//         path: "/dashboard",
-//         element: (
-//           <RequireAuth>
-//             <DashboardPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/process",
-//         loader: loader,
-//         element: (
-//           <RequireAuth>
-//             <StudentsPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/professors",
-//         loader: loader,
-//         element: (
-//           <RequireAuth>
-//             <ProfessorPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/students",
-//         loader: loader,
-//         element: (
-//           <RequireAuth>
-//             <StudentPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/edit-student/:id",
-//         loader: loader,
-//         element: (
-//           <RequireAuth>
-//             <EditStudentPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/create-professor",
-//         loader: loader,
-//         element: (
-//           <RequireAuth>
-//             <CreateProfessorPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/create-student",
-//         loader: loader,
-//         element: (
-//           <RequireAuth>
-//             <CreateStudentPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/studentProfile/:id",
-//         loader: getStudentProcess,
-//         element: (
-//           <RequireAuth>
-//             <ProcessInfoPage />
-//           </RequireAuth>
-//         ),
-//       },
-//       {
-//         path: "/createProcess",
-//         loader: loader,
-//         element: (
-//           <RequireAuth>
-//             <CreateProcessPage />
-//           </RequireAuth>
-//         ),
-//       },
-//     ],
-//   },
-//   {
-//     path: "/login",
-//     element: <LoginPage />,
-//   },
-//   {
-//     path: "*",
-//     element: <ErrorPage />,
-//   },
-// ]);
 
 function App() {
   return <RouterProvider router={router} />;
--- src/routes/ProtectedRoutes.tsx
+++ src/routes/ProtectedRoutes.tsx
@@ -1,21 +1,94 @@
 import Layout from "../layout/Layout";
+import CreateProcessPage from "../pages/CreateGraduation/CreateProcessPage";
 import { DashboardPage } from "../pages/dashboard/Dashboard";
+import ProcessInfoPage from "../pages/graduation/ProcessInfoPage";
+import CreateProfessorPage from "../pages/Professor/CreateProfessorPage";
+import ProfessorPage from "../pages/ProfessorPage";
+import CreateStudentPage from "../pages/Student/CreateStudentPage";
+import EditStudentPage from "../pages/Student/EditStudentPage";
+import StudentPage from "../pages/Student/StudentsPage";
+import StudentsPage from "../pages/StudentsPage";
 import RoleGuard from "./RoleGuard";
 
 const protectedRoutes = [
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-            {
-                path: '/dashboard',
-                element: (
-                    <RoleGuard allowedRoles={['admin', 'student']}>
-                        <DashboardPage />
-                    </RoleGuard>),
-            },
-        ]
-    }
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <DashboardPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/process",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <StudentsPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/professors",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <ProfessorPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/students",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <StudentPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/edit-student/:id",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <EditStudentPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/create-professor",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <CreateProfessorPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/create-student",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <CreateStudentPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/studentProfile/:id",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <ProcessInfoPage />
+          </RoleGuard>
+        ),
+      },
+      {
+        path: "/createProcess",
+        element: (
+          <RoleGuard allowedRoles={["admin", "student"]}>
+            <CreateProcessPage />
+          </RoleGuard>
+        ),
+      },
+    ],
+  },
 ];
 
 export default protectedRoutes;
